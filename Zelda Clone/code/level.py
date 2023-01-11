b2154859fee4718c5518845119d3ae731850cd1d
Diego de Carvalho <57546460+diegocarvalhossilva@users.noreply.github.com>
2023-01-11 18:14:25 -0300

    Add files via upload

---
+++ Zelda Clone/code/level.py
@@ -0,0 +1,64 @@
+import pygame
+from settings import *
+from tile import Tile
+from player import Player
+from debug import debug
+from support import *
+
+class Level:
+    def __init__(self):
+
+        self.display_surface = pygame.display.get_surface()
+        
+        #sprites
+        self.visible_sprites = YSortCameraGroup()
+        self.obstacles_sprites = pygame.sprite.Group()
+        self.create_map()
+
+    def create_map(self):
+        layout = {
+            'boundary': import_csv_layout('../map/map_FloorBlocks.csv')
+        }
+
+    for style, layout in layout.items():
+        for row_index,row in enumerate (WORLD_MAP):
+            for col_index, col in enumerate(row):
+                x = col_index * TILESIZE
+                y = row_index * TILESIZE
+            if style == 'boundary':
+                Tile((x,y),[self.visible_sprites, self.obstacle_sprites],'invisible')
+        #        if col == 'x':
+        #            Tile((x,y),[self.visible_sprites, self.obstacles_sprites])
+        #        if col == 'p':
+        #            self.player = Player((x,y),[self.visible_sprites], self.obstacles_sprites)
+        self.player = Player((400,300),[self.visible_sprites], self.obstacles_sprites)
+    
+    def run(self):
+        #atualiza a tela e renderiza os sprites
+        self.visible_sprites.custom_draw(self.player)
+        self.visible_sprites.update()
+
+class YSortCameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
+        self.offset = pygame.math.Vector2()
+
+        #Cria o chão
+        self.floor_surf = pygame.image.load('../graphics/tilemap/ground.png').convert()
+        self.floor_rect = self.floor_surf.get_rect(topleft = (0,0))
+
+    def custom_draw(self, player):
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+
+        #Desenha o chão
+        floor_offset_pos = self.floor_rect.topleft - self.offset
+        self.display_surface.blit(self.floor_surf, floor_offset_pos)
+
+        #for sprite in self.sprites():
+        for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image,offset_pos) #A imagem é desenhada no retângulo do sprite
