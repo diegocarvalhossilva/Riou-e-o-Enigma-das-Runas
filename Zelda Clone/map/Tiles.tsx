95f64b37d1824383fe16cce5c405f91a061f8dc0
Diego de Carvalho <57546460+diegocarvalhossilva@users.noreply.github.com>
2023-01-14 11:43:20 -0300

    Add files via upload

---
+++ Zelda Clone/map/Tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Tiles" tilewidth="64" tileheight="64" tilecount="20" columns="4">
+ <image source="Tiles.png" width="256" height="320"/>
+</tileset>
